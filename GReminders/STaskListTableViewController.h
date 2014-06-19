a04ecf3a3b9384b338efc7df92d8967312cc61e5
teeters <samteeter93@yahoo.com>
2014-06-19 08:28:44 -0500

    Initial commit. Set up GTL libraries to be compiled directly into project.

---
+++ GReminders/STaskListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  STaskListTableViewController.h
+//  GReminders
+//
+//  Created by Samuel Teeter on 6/18/14.
+//  Copyright (c) 2014 Samuel Teeter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STaskListTableViewController : UITableViewController
+
+@end
